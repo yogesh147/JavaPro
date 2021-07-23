227e5207340c0af0166a2b666cf613db0f0b35fe
yogesh147 <yogeshch147@gmail.com>
2021-07-23 23:30:22 +0530

    Thyme Leaf Demo Project

---
+++ javaPro/src/main/java/javaPro/thymeleaf/ThymeUserController.java
@@ -0,0 +1,24 @@
+package javaPro.thymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ThymeUserController {
+
+	@RequestMapping({ "/", "/index" })
+	public String index() {
+		return "index";
+	}
+
+	@GetMapping({ "/hello" })
+	public String hello(@RequestParam(value = "name", defaultValue = "World", required = true) String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "hello";
+	}
+
+}
